ef6b6cd1b3c9614f46a47b27c09deec3a5d4b897
sanjay salunkhe <sanjay.salunkhe@aylanetworks.com>
2022-08-09 01:30:12 +0530

    first commit

---
+++ lib/dummy_data_file.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dummy_data_file/version"
+
+module DummyDataFile
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dummy_data_file/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DummyDataFile
+  VERSION = "0.0.0"
+end
